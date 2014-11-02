5383cf12cb074782f1e5818d68c007f9505ee6ff
Matthias Hanisch <hanisch.matthias@googlemail.com>
2014-11-02 21:28:11 +0100

    - introduced cache used by DefaultEventSourceCollector

--- base-impl/src/main/java/de/javax/util/eventbinding/spi/impl/source/DefaultEventSourceCollector.java
+++ base-impl/src/main/java/de/javax/util/eventbinding/spi/impl/source/DefaultEventSourceCollector.java
@@ -2,14 +2,17 @@ package de.javax.util.eventbinding.spi.impl.source;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import de.javax.util.eventbinding.source.EventSourceFactory;
 import de.javax.util.eventbinding.spi.EventSource;
 import de.javax.util.eventbinding.spi.EventSourceCollector;
 import de.javax.util.eventbinding.spi.EventSourceId;
+import de.javax.util.eventbinding.spi.impl.source.EventSourceProviderClassInfo.EventSourceFieldInfo;
 
 /**
  * This implementation of an event source provider is based on sources which
@@ -22,6 +25,8 @@ import de.javax.util.eventbinding.spi.EventSourceId;
 public class DefaultEventSourceCollector implements EventSourceCollector {
     
     private final EventSourceFactory eventSourceFactory;
+    
+    private final Map<Class<?>, EventSourceProviderClassInfo> cache = new HashMap<Class<?>, EventSourceProviderClassInfo>();
     
     /**
      * Creates a new instance of this event source collector.
@@ -42,22 +47,66 @@ public class DefaultEventSourceCollector implements EventSourceCollector {
     }
 
     private Set<EventSource> findEventSources(Object eventSourceProvider, EventSourceId providerId) {
+        Class<?> eventSourceProviderClass = eventSourceProvider.getClass();
+        EventSourceProviderClassInfo info = this.cache.get(eventSourceProviderClass);
+        if(info==null) {
+            info = new EventSourceProviderClassInfo(this.collectFieldInfos(eventSourceProviderClass, providerId), collectNestedEventSourceProviders(eventSourceProviderClass, providerId));
+            cache.put(eventSourceProviderClass, info);
+        }
         Set<EventSource> collectedSources = new HashSet<EventSource>();
-        Field[] declaredFields = eventSourceProvider.getClass().getDeclaredFields();
-        for (Field declaredField : declaredFields) {
-            EventSourceId eventSourceFieldId = this.getEventSourceId(declaredField, providerId);
-            Object fieldValue = this.getFieldValue(declaredField, eventSourceProvider);
-            if (eventSourceFieldId != null && fieldValue != null) {
-                collectedSources.add(this.eventSourceFactory.createEventSource(eventSourceFieldId, fieldValue));
+        for(EventSourceFieldInfo fieldInfo:info.getFieldInfos()) {
+            Object fieldValue = this.getFieldValue(fieldInfo.getField(), eventSourceProvider);
+            if (fieldValue != null) {
+                collectedSources.add(this.eventSourceFactory.createEventSource(fieldInfo.getEventSourceId(), fieldValue));
             }
-            
-            EventSourceId eventSourceProviderId = this.getEventSourceProviderId(declaredField, providerId);
-            if (eventSourceProviderId != null && fieldValue != null) {
+        }
+        for(EventSourceFieldInfo nestedProvider:info.getNestedProviders()) {
+            Object fieldValue = this.getFieldValue(nestedProvider.getField(), eventSourceProvider);
+            if (fieldValue != null) {
                 // recursive call
-                collectedSources.addAll(this.findEventSources(fieldValue, eventSourceProviderId));
+                collectedSources.addAll(this.findEventSources(fieldValue, nestedProvider.getEventSourceId()));
             }
         }
         return collectedSources;
+    }
+    
+    /**
+     * Collects the nested event sources declared for a certain event source provider class.
+     * @param eventSourceProviderClass The class to check for event sources.
+     * @param providerId The event source id of the given event source provider class which will be the
+     * parent event source id of the event sources found. 
+     * @return A set of event sources encapsulated in an info object.
+     */
+    private Set<EventSourceFieldInfo> collectFieldInfos(Class<?> eventSourceProviderClass, EventSourceId providerId) {
+        Set<EventSourceFieldInfo> collectedFields = new HashSet<EventSourceFieldInfo>();
+        Field[] declaredFields = eventSourceProviderClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            EventSourceId eventSourceFieldId = this.getEventSourceId(declaredField, providerId);
+            if(eventSourceFieldId!=null){
+                collectedFields.add(new EventSourceFieldInfo(declaredField, eventSourceFieldId));
+            }
+        }
+        return collectedFields;
+    }
+    
+    
+    /**
+     * Collects the nested event source providers declared for a certain event source provider class.
+     * @param eventSourceProviderClass The class to check for nested event source providers.
+     * @param providerId The event source id of the given event source provider class which will be the
+     * parent event source id of the nested event source providers found. 
+     * @return A set of nested event source providers encapsulated in an info object.
+     */
+    private Set<EventSourceFieldInfo> collectNestedEventSourceProviders(Class<?> eventSourceProviderClass, EventSourceId providerId) {
+        Set<EventSourceFieldInfo> collectedFields = new HashSet<EventSourceFieldInfo>();
+        Field[] declaredFields = eventSourceProviderClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            EventSourceId eventSourceProviderId = this.getEventSourceProviderId(declaredField, providerId);
+            if (eventSourceProviderId != null) {
+                collectedFields.add(new EventSourceFieldInfo(declaredField, eventSourceProviderId));
+            }
+        }
+        return collectedFields;
     }
 
     private Object getFieldValue(Field field, Object object) {
@@ -116,4 +165,6 @@ public class DefaultEventSourceCollector implements EventSourceCollector {
         }
         return newId;
     }
+    
+    
 }
---
+++ base-impl/src/main/java/de/javax/util/eventbinding/spi/impl/source/EventSourceProviderClassInfo.java
@@ -0,0 +1,48 @@
+package de.javax.util.eventbinding.spi.impl.source;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import de.javax.util.eventbinding.spi.EventSourceId;
+
+/**
+ * Encapsulates the information about an event source provider object.
+ * @author Matthias Hanisch
+ *
+ */
+class EventSourceProviderClassInfo {
+    private Set<EventSourceFieldInfo> fieldInfos;
+    private Set<EventSourceFieldInfo> nestedProviders;
+    EventSourceProviderClassInfo(Set<EventSourceFieldInfo> fieldInfos, Set<EventSourceFieldInfo> nestedProviders) {
+        this.fieldInfos = fieldInfos;
+        this.nestedProviders = nestedProviders;
+    }
+    public Set<EventSourceFieldInfo> getFieldInfos() {
+        return fieldInfos;
+    }
+    
+    public Set<EventSourceFieldInfo> getNestedProviders() {
+        return nestedProviders;
+    }
+    /**
+     * Encapsulates the information about a field of an event source provider object.
+     * @author Matthias Hanisch
+     *
+     */
+    public static class EventSourceFieldInfo {
+        private final Field field;
+        private final EventSourceId eventSourceId;
+        EventSourceFieldInfo(Field field, EventSourceId eventSourceId) {
+            this.field = field;
+            this.eventSourceId = eventSourceId;
+        }
+        
+        public Field getField() {
+            return field;
+        }
+        
+        public EventSourceId getEventSourceId() {
+            return eventSourceId;
+        }
+    }
+}
